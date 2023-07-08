3cfbf2a4b38e3b6a52ecda544ef3b11b6ddaca10
braulioPad <64759065+braulioPad@users.noreply.github.com>
2023-07-08 18:41:01 +0900

    first commit

---
+++ src/main/java/com/java/token/service/JwtUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.java.token.service;
+
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if ("javainuse".equals(username)) {
+			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+					new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+
+	public String getClave(String api) {
+		if(api.equalsIgnoreCase("api01")) {
+			return "clave1";
+		}else {
+			return "clave2";
+		}
+	}
+
+}
